672e73fb73a2aae740f4a86571ad296e0b8563da
zerodiv <narz.artem@gmail.com>
2026-02-18 23:52:08 +0300

    chore: initial project structure and dependencies

---
+++ cmd/heimdallr/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+func main() {
+	log.Println("heimdallr-proxy is running...")
+
+	// Тут в будущем мы запустим:
+	// 1. Подключение к БД
+	// 2. gRPC клиент к Xray
+	// 3. Telegram бот
+	// 4. API сервер для Vue
+
+	fmt.Println("Heimdallr: proxy is ready to handle requests.")
+}
