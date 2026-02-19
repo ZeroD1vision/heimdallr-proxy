d0723f23e49358969f36da2d47bff10968e0da47
zerodiv <narz.artem@gmail.com>
2026-02-19 22:02:06 +0300

    feat(config): implement environment-based configuration and project skeleton

--- cmd/heimdallr/main.go
+++ cmd/heimdallr/main.go
@@ -1,12 +1,20 @@
 package main
 
 import (
-	"fmt"
 	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
 	log.Println("heimdallr-proxy is running...")
+	err := godotenv.Load()
+	if err != nil {
+		val := os.Getenv("APP_ENV")
+		log.Printf("Значение APP_ENV из системы: [%s]", val)
+		log.Fatalf("Критическая ошибка: %v", err) // Если файла нет, программа тут же остановится с ошибкой
+	}
 
 	// Тут в будущем мы запустим:
 	// 1. Подключение к БД
@@ -14,5 +22,10 @@ func main() {
 	// 3. Telegram бот
 	// 4. API сервер для Vue
 
-	fmt.Println("Heimdallr: proxy is ready to handle requests.")
+	// fmt.Println("Heimdallr: proxy is ready to handle requests.")
+
+	if os.Getenv("APP_ENV") == "local" {
+		log.Println("Running in local environment. Downloading .env file...")
+		log.Printf("Starting Heimdallr in %s mode...", os.Getenv("APP_ENV"))
+	}
 }
