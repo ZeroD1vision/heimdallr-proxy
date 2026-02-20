f9513d6b26d0e0c7b104cd85fedd5cfcc8fe2d08
zerodiv <narz.artem@gmail.com>
2026-02-20 23:32:37 +0300

    feat(xray): implement gRPC client for user traffic statistics
    Connects to Xray API via gRPC and fetches downlink metrics for zd_pc user. Integrated godotenv for port configuration.

--- cmd/heimdallr/main.go
+++ cmd/heimdallr/main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/joho/godotenv"
+	"github.com/xtls/xray-core/app/stats/command"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 func main() {
@@ -12,8 +18,8 @@ func main() {
 	err := godotenv.Load()
 	if err != nil {
 		val := os.Getenv("APP_ENV")
-		log.Printf("Значение APP_ENV из системы: [%s]", val)
-		log.Fatalf("Критическая ошибка: %v", err) // Если файла нет, программа тут же остановится с ошибкой
+		log.Printf("APP_ENV in system: [%s]", val)
+		log.Printf("Error: %v", err)
 	}
 
 	// Тут в будущем мы запустим:
@@ -22,10 +28,31 @@ func main() {
 	// 3. Telegram бот
 	// 4. API сервер для Vue
 
-	// fmt.Println("Heimdallr: proxy is ready to handle requests.")
+	conn, err := grpc.NewClient(fmt.Sprintf("127.0.0.1:%s", os.Getenv("SERVER_TUNNEL_PORT")), grpc.WithTransportCredentials(insecure.NewCredentials()))
 
-	if os.Getenv("APP_ENV") == "local" {
-		log.Println("Running in local environment. Downloading .env file...")
-		log.Printf("Starting Heimdallr in %s mode...", os.Getenv("APP_ENV"))
+	if err != nil {
+		log.Fatalf("Failed to connect to Xray gRPC API: %v", err)
 	}
+
+	defer conn.Close()
+
+	client := command.NewStatsServiceClient(conn)
+
+	req := &command.GetStatsRequest{
+		Name:   "user>>>zd_pc>>>traffic>>>downlink",
+		Reset_: false,
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+
+	fmt.Println("Requesting stats from Xray gRPC API...")
+	res, err := client.GetStats(ctx, req)
+
+	if err != nil {
+		log.Fatalf("✘ Error getting stats: %v", err)
+	}
+
+	mb := float64(res.Stat.Value) / 1024 / 1024
+	fmt.Printf("✔ Success!\nUser: zd_pc\nTraffic (downlink): %.2f MB (%d bytes)\n", mb, res.Stat.Value)
 }
