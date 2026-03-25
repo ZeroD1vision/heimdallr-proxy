17ccfe601bbcf9244073d961285fdac13c8b3336
zerodiv <narz.artem@gmail.com>
2026-03-25 11:39:44 +0300

    chore(db): swap to glebarez/sqlite to support CGO_ENABLED=0

--- internal/db/store.go
+++ internal/db/store.go
@@ -6,7 +6,7 @@ import (
 	"path/filepath"
 
 	"github.com/ZeroD1vision/heimdallr-proxy/internal/models"
-	"gorm.io/driver/sqlite"
+	"github.com/glebarez/sqlite"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
 )
